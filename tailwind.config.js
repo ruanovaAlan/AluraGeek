b4c2e4622960d60a24e803a2416f4ee3524409bb
Alan Ruanova <ruanovaalan96@gmail.com>
2024-05-18 18:48:12 -0600

    Add Google Fonts and update styling

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,7 +5,12 @@ export default {
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        'title': ['Permanent Marker', 'cursive'],
+        'body': ['Sen', 'sans-serif'],
+      },
+    },
   },
   plugins: [],
 }
